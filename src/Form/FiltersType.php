4514f28abc3f14f4f023c6d7fea8d93c61592780
Gaetan <tubaro.gaetan@gmail.com>
2022-03-21 16:21:41 +0100

    finition des filtres

--- src/Form/FiltersType.php
+++ src/Form/FiltersType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -40,8 +41,7 @@ class FiltersType extends AbstractType
             'required' => false,
         ])
         ->add('submit', SubmitType::class, [
-            'label' => 'Rechercher'])
-        ;
+            'label' => 'Rechercher']);
     }   
 
     public function configureOptions(OptionsResolver $resolver): void
